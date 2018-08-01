a835de2de4f009adb22d8f46dfe2f12ac577f6a2
Juan Castaño <[juan.castano@col.vueling.com]>
2018-08-01 08:09:05 -0500

    Added PonyORM and bcrypt libraries

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
 
 requires = [
+    'bcrypt',
+    'pony',
     'plaster_pastedeploy',
     'pyramid',
     'pyramid_jinja2',
