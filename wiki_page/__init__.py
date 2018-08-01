034692b2214c3e9e445e05475d5e4c2fc452c429
Juan Castaño <[juan.castano@col.vueling.com]>
2018-08-01 13:57:47 -0500

    Added database connection

--- wiki_page/__init__.py
+++ wiki_page/__init__.py
@@ -1,5 +1,19 @@
 from pyramid.config import Configurator
 
+from pony.orm import Database
+
+# Define manager database
+database = Database()
+
+def __initialize_database():
+    """Generate new database instance single reference for managing database"""
+    print("Generating database")
+    try:
+        database.bind(provider='sqlite', filename='wiki_page.sqlite', create_db=True)
+        database.generate_mapping(create_tables=True)
+    except Exception as ex:
+        print("Unexpected error: {}".format(ex))
+    
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
@@ -9,4 +23,5 @@ def main(global_config, **settings):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.scan()
+    __initialize_database()
     return config.make_wsgi_app()
